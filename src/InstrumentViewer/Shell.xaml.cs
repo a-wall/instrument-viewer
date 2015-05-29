b334bdb80493c5ce1adedd791147579b7a60aadb
glebivanov <ivanov.gleb@gmail.com>
2015-05-29 23:55:58 +0100

    MahApps.Metro added

--- src/InstrumentViewer/Shell.xaml.cs
+++ src/InstrumentViewer/Shell.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace InstrumentViewer
 {
     /// <summary>
     /// Interaction logic for Shell.xaml
     /// </summary>
-    public partial class Shell : Window
+    public partial class Shell : MetroWindow
     {
         public Shell()
         {
