afcfcd95d685dc143276d51b9997ff0a3ea6f0df
glebivanov <ivanov.gleb@gmail.com>
2015-05-29 07:11:26 +0100

    InstrumentGridView added

--- src/InstrumentViewer/Instrument.Ui/InstrumentUiModule.cs
+++ src/InstrumentViewer/Instrument.Ui/InstrumentUiModule.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Instrument.Ui.Views;
 using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
 
 namespace Instrument.Ui
 {
     public class InstrumentUiModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public InstrumentUiModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void Initialize()
         {
-            
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(InstrumentGridView));
         }
     }
 }
