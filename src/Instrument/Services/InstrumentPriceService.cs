76233d6e6709ff1cb963f0b2a19d5aa9e5da71c2
glebivanov <ivanov.gleb@gmail.com>
2015-05-30 18:29:09 +0100

    Throttle prices

--- src/Instrument/Services/InstrumentPriceService.cs
+++ src/Instrument/Services/InstrumentPriceService.cs
@@ -3,13 +3,14 @@ using System.Reactive.Linq;
 
 namespace Instrument.Services
 {
-    public class InstrumentPriceService : IInstrumentPriceService
+    public sealed class InstrumentPriceService : IInstrumentPriceService
     {
         readonly Random _random = new Random();
         
         public IObservable<InstrumentPrice> ObserveInstrumentPrices()
         {
-            return Observable.Generate(100, _ => true, i => i + _random.Next(-1, 2), i => new InstrumentPrice("VOD.L", i), _ => TimeSpan.FromMilliseconds(100));
+            return Observable.Generate(100, _ => true, i => i + _random.Next(-1, 2), i => new InstrumentPrice("VOD.L", i), _ => TimeSpan.FromMilliseconds(100))
+                             .Merge(Observable.Generate(70, _ => true, i => i + _random.Next(-1, 2), i => new InstrumentPrice("BT.L", i), _ => TimeSpan.FromMilliseconds(100)));
         }
     }
 }
---
+++ src/Shared/ObservableExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace Shared
+{
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Applies a conflation algorithm to an observable stream. 
+        /// Anytime the stream OnNext twice below minimumUpdatePeriod, the second update gets delayed to respect the minimumUpdatePeriod
+        /// If more than 2 update happen, only the last update is pushed
+        /// Updates are pushed and rescheduled using the provided scheduler
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">stream</param>
+        /// <param name="minimumUpdatePeriod">minimum delay between 2 updates</param>
+        /// <param name="scheduler">to be used to publish updates and schedule delayed updates</param>
+        /// <returns></returns>
+        public static IObservable<T> Conflate<T>(this IObservable<T> source, TimeSpan minimumUpdatePeriod, IScheduler scheduler)
+        {
+            return Observable.Create<T>(observer =>
+            {
+                // indicate when the last update was published
+                var lastUpdateTime = DateTimeOffset.MinValue;
+                // indicate if an update is currently scheduled
+                var updateScheduled = new MultipleAssignmentDisposable();
+                // indicate if completion has been requested (we can't complete immediatly if an update is in flight)
+                var completionRequested = false;
+                var gate = new object();
+
+                var subscription = source
+                        .ObserveOn(scheduler)
+                        .Subscribe(
+                            x =>
+                            {
+                                var currentUpdateTime = scheduler.Now;
+
+                                bool scheduleRequired;
+                                lock (gate)
+                                {
+                                    scheduleRequired = currentUpdateTime - lastUpdateTime < minimumUpdatePeriod;
+                                    if (scheduleRequired && updateScheduled.Disposable != null)
+                                    {
+                                        updateScheduled.Disposable.Dispose();
+                                        updateScheduled.Disposable = null;
+                                    }
+                                }
+
+                                if (scheduleRequired)
+                                {
+                                    updateScheduled.Disposable = scheduler.Schedule(lastUpdateTime + minimumUpdatePeriod, () =>
+                                    {
+                                        observer.OnNext(x);
+
+                                        lock (gate)
+                                        {
+                                            lastUpdateTime = scheduler.Now;
+                                            updateScheduled.Disposable = null;
+                                            if (completionRequested)
+                                            {
+                                                observer.OnCompleted();
+                                            }
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    observer.OnNext(x);
+                                    lock (gate)
+                                    {
+                                        lastUpdateTime = scheduler.Now;
+                                    }
+                                }
+                            },
+                            observer.OnError,
+                            () =>
+                            {
+                                // if we have scheduled an update we need to complete once the update has been published
+                                if (updateScheduled.Disposable != null)
+                                {
+                                    lock (gate)
+                                    {
+                                        completionRequested = true;
+                                    }
+                                }
+                                else
+                                {
+                                    observer.OnCompleted();
+                                }
+                            });
+
+                return subscription;
+            });
+        }
+    }
+}
