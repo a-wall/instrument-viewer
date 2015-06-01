b1ced5930846ba2c5d8ad913a7a26036379bfed3
glebivanov <ivanov.gleb@gmail.com>
2015-06-01 21:46:18 +0100

    One extra unit test for viewcontroller

--- src/InstrumentViewer.Tests/Instrument.Ui/ViewControllers/InstrumentGridViewControllerTests.cs
+++ src/InstrumentViewer.Tests/Instrument.Ui/ViewControllers/InstrumentGridViewControllerTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Instrument;
 using Instrument.Services;
 using Instrument.Ui.ViewControllers;
 using Microsoft.Reactive.Testing;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Shared;
 
 namespace InstrumentViewer.Tests.Instrument.Ui.ViewControllers
 {
@@ -69,6 +71,37 @@ namespace InstrumentViewer.Tests.Instrument.Ui.ViewControllers
             Assert.AreEqual(1, viewController.ViewModel.InstrumentPrices.Count);
             Assert.AreEqual(103, viewController.ViewModel.InstrumentPrices[0].CurrentPrice.Value);
             Assert.AreEqual(102, viewController.ViewModel.InstrumentPrices[0].AveragePrice.Value);
+        }
+
+        [Test]
+        public void MultipleUpdatesWithinThrottleIntervalShouldUpdateViewModelJustOnce()
+        {
+            // Given
+            var testScheduler = new TestScheduler();
+            var instrumentPricesSubject = new Subject<InstrumentPrice>();
+            var instrumentPriceService = MockRepository.GenerateStrictMock<IInstrumentPriceService>();
+            instrumentPriceService.Expect(s => s.ObserveInstrumentPrices())
+                                  .Return(instrumentPricesSubject);
+
+            // When
+            var viewController = new InstrumentGridViewController(instrumentPriceService, _conflateTimeSpan, _ => { }, ImmediateScheduler.Instance, testScheduler);
+            viewController.Initialize();
+            testScheduler.Start();
+            instrumentPricesSubject.OnNext(new InstrumentPrice("VOD.L", 100));
+            testScheduler.Start();
+            var numberOfPriceUpdates = 0;
+            viewController.ViewModel.InstrumentPrices[0].CurrentPrice.ObservePropertyChanged()
+                                                                     .Where(x => x.EventArgs.PropertyName.Equals("Value"))
+                                                                     .Subscribe(_ => numberOfPriceUpdates++);
+            instrumentPricesSubject.OnNext(new InstrumentPrice("VOD.L", 101));
+            instrumentPricesSubject.OnNext(new InstrumentPrice("VOD.L", 102));
+            instrumentPricesSubject.OnNext(new InstrumentPrice("VOD.L", 103));
+            testScheduler.Start();
+            
+            // Then
+            Assert.AreEqual(1, viewController.ViewModel.InstrumentPrices.Count);
+            Assert.AreEqual(1, numberOfPriceUpdates);
+            Assert.AreEqual(103, viewController.ViewModel.InstrumentPrices[0].CurrentPrice.Value);
         }
     }
 }
---
+++ src/Shared/PropertyChangedExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Shared
+{
+    public static class PropertyChangedExtensions
+    {
+        public static IObservable<EventPattern<PropertyChangedEventArgs>> ObservePropertyChanged(this INotifyPropertyChanged source)
+        {
+            return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(h => source.PropertyChanged += h, h => source.PropertyChanged -= h);
+        }
+    }
+}
