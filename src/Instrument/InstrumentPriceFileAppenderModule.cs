61e5ed188c8c700c19620f1e1a452ed60ad15e61
glebivanov <ivanov.gleb@gmail.com>
2015-06-01 21:49:13 +0100

    Generate test prices every 100ms

--- src/Instrument/InstrumentPriceFileAppenderModule.cs
+++ src/Instrument/InstrumentPriceFileAppenderModule.cs
@@ -20,11 +20,11 @@ namespace Instrument
 
             var random = new Random();
 
-            var prices = Observable.Generate(95, _ => true, i => i + random.Next(-1, 2), i => new InstrumentPrice("VOD.L", i), _ => TimeSpan.FromMilliseconds(1000))
-                                   .Merge(Observable.Generate(97, _ => true, i => i + random.Next(-1, 2), i => new InstrumentPrice("BT.L", i), _ => TimeSpan.FromMilliseconds(1000)))
-                                   .Merge(Observable.Generate(1450.1M, _ => true, i => i + (decimal)random.Next(-1, 2) / 10, i => new InstrumentPrice("BP.L", i), _ => TimeSpan.FromMilliseconds(1000)))
-                                   .Merge(Observable.Generate(12830.2M, _ => true, i => i + (decimal)random.Next(-1, 2) / 10, i => new InstrumentPrice("Dow", i), _ => TimeSpan.FromMilliseconds(1000)))
-                                   .Merge(Observable.Generate(85.6M, _ => true, i => i + (decimal)random.Next(-1, 2) / 10, i => new InstrumentPrice("Oil", i), _ => TimeSpan.FromMilliseconds(1000)));
+            var prices = Observable.Generate(95, _ => true, i => i + random.Next(-1, 2), i => new InstrumentPrice("VOD.L", i), _ => TimeSpan.FromMilliseconds(100))
+                                   .Merge(Observable.Generate(97, _ => true, i => i + random.Next(-1, 2), i => new InstrumentPrice("BT.L", i), _ => TimeSpan.FromMilliseconds(100)))
+                                   .Merge(Observable.Generate(1450.1M, _ => true, i => i + (decimal)random.Next(-1, 2) / 10, i => new InstrumentPrice("BP.L", i), _ => TimeSpan.FromMilliseconds(100)))
+                                   .Merge(Observable.Generate(12830.2M, _ => true, i => i + (decimal)random.Next(-1, 2) / 10, i => new InstrumentPrice("Dow", i), _ => TimeSpan.FromMilliseconds(100)))
+                                   .Merge(Observable.Generate(85.6M, _ => true, i => i + (decimal)random.Next(-1, 2) / 10, i => new InstrumentPrice("Oil", i), _ => TimeSpan.FromMilliseconds(100)));
 
             Observable.Using(() => new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.ReadWrite),
                                    fs => Observable.Using(() => new StreamWriter(fs) { AutoFlush = true },
