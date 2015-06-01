631e8ff3d66a0c88f9699723b33af523c2af7f2e
glebivanov <glebivanov@users.noreply.github.com>
2015-06-01 15:33:17 +0100

    Dispose controllers on App Exit

--- src/Instrument.Ui/InstrumentUiModule.cs
+++ src/Instrument.Ui/InstrumentUiModule.cs
@@ -14,12 +14,14 @@ namespace Instrument.Ui
         private readonly IRegionManager _regionManager;
         private readonly IInstrumentPriceService _instrumentPriceService;
         private readonly IDialogService _dialogService;
+        private readonly CompositeDisposable _disposable;
 
         public InstrumentUiModule(IRegionManager regionManager, IInstrumentPriceService instrumentPriceService, IDialogService dialogService)
         {
             _regionManager = regionManager;
             _instrumentPriceService = instrumentPriceService;
             _dialogService = dialogService;
+            _disposable = new CompositeDisposable();
         }
 
         public void Initialize()
@@ -28,7 +30,12 @@ namespace Instrument.Ui
             // There's no need to show every single price change on the client
             var viewController = new InstrumentGridViewController(_instrumentPriceService, TimeSpan.FromMilliseconds(300), ShowHistoricalPrices, DispatcherScheduler.Current, new EventLoopScheduler());
             viewController.Initialize();
+            viewController.AddToDisposable(_disposable);
             _regionManager.RegisterViewWithRegion("MainRegion", () => new InstrumentGridView {DataContext = viewController.ViewModel});
+
+            Observable.FromEventPattern<ExitEventHandler, ExitEventArgs>(h => Application.Current.Exit += h, h => Application.Current.Exit -= h)
+                      .Take(1)                      
+                      .Subscribe(_ => _disposable.Dispose());
         }
 
         private void ShowHistoricalPrices(string instrument)
@@ -36,8 +43,9 @@ namespace Instrument.Ui
             // We throttle the updates (300ms) to avoid extra load on the dispatcher
             var historicalPricesViewController = new InstrumentHistoricalPricesViewController(_instrumentPriceService, TimeSpan.FromMilliseconds(300), DispatcherScheduler.Current, TaskPoolScheduler.Default);
             historicalPricesViewController.Initialize(instrument);
+            historicalPricesViewController.AddToDisposable(_disposable);
             var historicalPricesView = new InstrumentHistoricalPricesView();
-            _dialogService.ShowDialog(historicalPricesView, historicalPricesViewController.ViewModel);
+            _dialogService.ShowDialog(historicalPricesView, historicalPricesViewController.ViewModel); // there should be some callback notifying the dialog is closed to dispose historicalPricesViewController
         }
     }
 }
